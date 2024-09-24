8c79e164185c35794498be74543fd7eb6f3a0741
Oscar Patterson <oepatterson12@gmail.com>
2024-09-24 13:44:33 -0400

    g

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,5 +1,5 @@
 class BikeClub
-
+    attr_reader :name, :bikers
 
     def initialize(name)
         @name = name
@@ -12,11 +12,22 @@ class BikeClub
     end
      
     def most_rides
+        @bikers.max_by do |biker| #finds biker with most logged rides
+
+            biker.rides.keys.size
+            #returns the amount of keys the biggewst biker has(different rides)
+        end
+    end
+
 
     end
 
     def best_time(ride)
+        personalrecords = {}
+        @bikers.each do |biker|
 
+            #biker personal record for given ride put it in as their name as the key, 
+            #the record as the value and then find the highest value and return 
     end
 
     def bikers_eligible(ride)
