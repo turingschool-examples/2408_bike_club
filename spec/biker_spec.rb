fe3ff764d10983e2fcd5b97774950f5273622d27
Paul Knapp <ppankluap@gmail.com>
2024-09-24 10:50:19 -0500

    test: create initialize tests

---
+++ spec/biker_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+RSpec.describe Biker do 
+    before(each:) do 
+        @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+        @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+        @biker1 = Biker.new("Kenny", 30)
+        @biker2 = Biker.new("Athena", 15)
+    end
+
+    desribe 'initialize' do 
+        it 'exists' do
+            expect(@biker1).to be_an_instance_of(Biker)
+            expect(@biker2).to be_an_instance_of(Biker)
+        end
+
+        it 'has attributes' do
+            expect(@biker1.name).to eq("Kenny")
+            expect(@biker2.name).to eq("Athena")
+            expect(@biker1.max_distance).to eq(30)
+            expect(@biker2.max_distance).to eq(15)
+            expect(@biker1.rides).to eq({})
+            expect(@biker2.rides).to eq({})
+            expect(@biker1.acceptable_terrain).to eq([])
+            expect(@biker2.acceptable_terrain).to eq([])
+        end
+    end
+end
