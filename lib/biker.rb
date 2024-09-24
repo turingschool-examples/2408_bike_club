e77f9ebbb4644594379baf523805bbec6bc79373
Seth Verrill <verrill.seth@gmail.com>
2024-09-24 09:51:35 -0600

    feat: Add log ride and can do ride? methods

--- lib/biker.rb
+++ lib/biker.rb
@@ -14,4 +14,18 @@ class Biker
   def learn_terrain!(terrain)
     @acceptable_terrain << terrain
   end
+
+  def log_ride(ride, time)
+    if can_do_ride?(ride)
+      if @rides[ride]
+        @rides[ride] << time
+      else
+        @rides[ride] = [time]
+      end
+    end
+  end
+
+  def can_do_ride?(ride)
+    @acceptable_terrain.include?(ride.terrain) && ride.total_distance <= @max_distance
+  end
 end
