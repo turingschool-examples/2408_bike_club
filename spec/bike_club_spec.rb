38d1ae500ba0b49fca1a06fb571eb4d32f9ec556
Wally Wallace <jrvw001@gmail.com>
2024-09-24 11:00:43 -0600

    Add test for most_rides

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe BikeClub do
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
     @biker1 = Biker.new("Kenny", 30)
     @biker2 = Biker.new("Wally", 15)
+    @biker1.learn_terrain!(:gravel)
+    @biker1.learn_terrain!(:hills)
+    @biker2.learn_terrain!(:gravel)
+    @biker2.learn_terrain!(:hills)
   end
 
   describe '#initialize' do
@@ -24,8 +28,21 @@ RSpec.describe BikeClub do
       expect(@bike_club1.bikers).to eq([])
       @bike_club1.add_biker(@biker1)
       expect(@bike_club1.bikers).to eq([@biker1])
-      @bike_club1.add_biker(@biker1)
+      @bike_club1.add_biker(@biker2)
       expect(@bike_club1.bikers).to eq([@biker1, @biker2])
+    end
+  end
+
+  describe '#most_rides' do
+    it 'can return the rider with the most rides' do
+      @biker1.log_ride(@ride1, 100.1)
+      @biker1.log_ride(@ride2, 44.4)
+      @biker2.log_ride(@ride2, 63.0)
+      @biker2.log_ride(@ride2, 61.9)
+      @biker2.log_ride(@ride2, 62.6)
+      @bike_club1.add_biker(@biker1)
+      @bike_club1.add_biker(@biker2)
+      expect(@bike_club1.most_rides).to eq(@biker2)
     end
   end
 end
