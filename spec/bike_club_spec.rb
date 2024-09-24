8dd0a050fab8fcd1cde7afb4967d53811751b987
Wally Wallace <jrvw001@gmail.com>
2024-09-24 10:52:41 -0600

    Create test for add_biker

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -4,14 +4,28 @@ require './lib/biker'
 
 RSpec.describe BikeClub do
   before(:each) do
-    @bikeclub1 = BikeClub.new('BikeClub1')
+    @bike_club1 = BikeClub.new('Bike_Club1')
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    @biker1 = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Wally", 15)
   end
 
   describe '#initialize' do
     it 'exists' do
-      expect(@bikeclub1).to be_an_instance_of(BikeClub)
-      expect(@bikeclub1.name).to eq('BikeClub1')
-      expect(@bikeclub1.bikers).to eq([])
+      expect(@bike_club1).to be_an_instance_of(BikeClub)
+      expect(@bike_club1.name).to eq('Bike_Club1')
+      expect(@bike_club1.bikers).to eq([])
+    end
+  end
+
+  describe '#add_biker' do
+    it 'can add a biker' do
+      expect(@bike_club1.bikers).to eq([])
+      @bike_club1.add_biker(@biker1)
+      expect(@bike_club1.bikers).to eq([@biker1])
+      @bike_club1.add_biker(@biker1)
+      expect(@bike_club1.bikers).to eq([@biker1, @biker2])
     end
   end
 end
