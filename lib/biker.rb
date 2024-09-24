0a98496e4bbc159ab38c0fc325c81467ff480aa8
Wally Wallace <jrvw001@gmail.com>
2024-09-24 10:05:15 -0600

    Add check for acceptable terrain

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,3 +1,31 @@
 class Biker
 
+  attr_reader :name, :max_distance, :rides, :acceptable_terrain
+  
+  def initialize(name, max_distance)
+    @name = name
+    @max_distance = max_distance
+    @rides = Hash.new {|rides, times| rides[times] = []}
+    @acceptable_terrain = []
+  end
+
+  def learn_terrain!(terrain)
+    @acceptable_terrain << terrain
+  end
+
+  def log_ride(ride, time)
+    if @acceptable_terrain.include?(ride.terrain)
+    @rides[ride] << time
+    end
+  end
+
+  def personal_record(ride)
+    if @rides.key?(ride)
+      times = @rides[ride]
+      times.min
+    else
+      false
+    end
+    
+  end
 end
