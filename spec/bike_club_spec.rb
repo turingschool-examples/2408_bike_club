ca4eb4e341d9d9a927616cb1eef1317b90d8e98c
Seth Verrill <verrill.seth@gmail.com>
2024-09-24 11:26:55 -0600

    test: Add test to verify empty starting array, add ineligible rider, and fix typos

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -73,13 +73,16 @@ RSpec.describe BikeClub do
       @biker2.learn_terrain!(:gravel)
     end
     it 'records group ride with start time for eligible members' do
-      allow(Date).to receive(:today).and_return(Date.new(2005, 10, 31))
-      allow(Time).to receive(:now).and_return(Time.new(2005, 10, 31, 08, 00, 30))
+      expect(@club.group_rides).to eq([])
 
-      finish = @club.record_group_ride(ride)
+      allow(Date).to receive(:today).and_return(Date.new(2005, 10, 31))
+      allow(Time).to receive(:now).and_return(Time.new(2005, 10, 31, 8, 00, 30))
+
+      finish = @club.record_group_ride(@ride2)
       expect(finish[:start_time].strftime("%m/%d/%Y %H:%M:%S")).to eq("10/31/2005 08:00:30")
-      expect(finish[:ride]).to eq(ride)
+      expect(finish[:ride]).to eq(@ride2)
       expect(finish[:members]).to eq([@biker1, @biker2])
       expect(finish[:members].include?(@biker3)).to eq(false)
     end
+  end
 end
