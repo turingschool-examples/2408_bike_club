5429554e01b72130fdeac9ccaa75699400f66c0f
Alora Riley <alora.riley@gmail.com>
2024-09-24 09:29:22 -0600

    Add initialize biker method

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,3 +1,8 @@
 class Biker
-
+  attr_reader :name,
+              :max_distance
+  def initialize(name, max_distance)
+    @name = name
+    @max_distance = max_distance
+  end
 end
---
+++ spec/biker_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe Biker do
+  before(:each) do
+    @biker = Biker.new("Kenny", 30)
+    # @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    # @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@biker).to be_an_instance_of(Biker)
+    end
+
+    it 'has a name' do
+      expect(@biker.name).to eq("Kenny")
+    end
+
+    it 'has a max distance' do 
+      expect(@biker.max_distance).to eq(30)
+    end
+  end
+end
