d19b5ae32432256c9ae3d05dbc04979d08e5e681
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-24 09:53:13 -0600

    added #learn_terrain, tests for biker exist and has attributes

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -1,6 +1,24 @@
 require './lib/biker'
+require 'pry'
 
 RSpec.describe Biker do
-  it' exists and has attributes' do
+  before(:each) do
+    @biker = Biker.new("Kenny", 30)
+  end
+  
+    it' exists and has attributes' do
+      expect(@biker).to be_instance_of(Biker)
+      expect(@biker.name).to eq("Kenny")
+      expect(@biker.max_distance).to eq(30)
+      expect(@biker.rides).to eq({})
+      expect(@biker.acceptable_terrain).to eq([])
+    end
+  
+  describe '#learn_terrain!' do
+    it 'allows biker to learn terrain' do
+      (@biker.learn_terrain!(:gravel))
+      (@biker.learn_terrain!(:hills))
+      expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
   end
 end
