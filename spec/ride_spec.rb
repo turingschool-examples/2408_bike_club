b225f0971e8110120d19a95de9ef8ae44175ae2b
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-24 09:28:24 -0600

    added ride2 tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -3,12 +3,22 @@ require './lib/ride'
 RSpec.describe Ride do 
   before(:each) do
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
+  
     it ' exists and has attributes' do
-    expect(@ride1).to be_instance_of(Ride)
-    expect(@ride1.name).to eq("Walnut Creek Trail")
-    expect(@ride1.distance).to eq(10.7)
-    expect(@ride1.loop?).to eq(false)
-    expect(@ride1.terrain).to eq(:hills)
-  end
+      expect(@ride1).to be_instance_of(Ride)
+      expect(@ride1.name).to eq("Walnut Creek Trail")
+      expect(@ride1.distance).to eq(10.7)
+      expect(@ride1.loop?).to eq(false)
+      expect(@ride1.terrain).to eq(:hills)
+    end
+
+    it 'shows bifferent attributes for a 2nd ride' do
+      expect(@ride2).to be_instance_of(Ride)
+      expect(@ride2.name).to eq("Town Lake")
+      expect(@ride2.distance).to eq(14.9)
+      expect(@ride2.loop?).to eq(true)
+      expect(@ride2.terrain).to eq(:gravel)
+    end
 end
