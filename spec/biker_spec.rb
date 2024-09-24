fd24ab203ac6dab02bd3e2720d24c4b01b209fb3
Seth Verrill <verrill.seth@gmail.com>
2024-09-24 09:29:35 -0600

    test: Add test for learn terrain

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -3,7 +3,8 @@ require './spec/spec_helper'
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
-
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
   describe 'biker' do
@@ -11,8 +12,20 @@ RSpec.describe Biker do
       expect(@biker).to be_an_instance_of(Biker)
       expect(@biker.name).to eq("Kenny")
       expect(@biker.max_distance).to eq(30)
-      expect(@biker.rides).to eq([])
+      expect(@biker.rides).to eq({})
       expect(@biker.acceptable_terrain).to eq([])
     end
   end
+
+  describe 'learn terrain' do
+    it 'can learn new terrain' do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+      expect(@biker.acceptable_terrain).to eq([:gravel, hills])
+    end
+  end
+
+
+
+
 end
