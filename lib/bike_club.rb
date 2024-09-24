e8e4270c323197bab85a884d041ee68d182a7995
Devlin Lynch <devklynch@gmail.com>
2024-09-24 10:15:41 -0600

    feat: add starting methods for BikeClub

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,3 +1,26 @@
 class BikeClub
 
+    attr_reader :name,
+                :bikers
+
+    def initialize(name)
+        @name = name
+        @bikers = []
+    end
+
+    def add_biker(biker)
+        @bikers << biker
+    end
+
+    def most_rides
+        ride_count_hash = {}
+        @bikers.map do |biker|
+            biker.rides.map do |ride| 
+                ride_count_hash[biker] ||= 0
+                    ride_count_hash[biker] += ride[1].count
+            end
+        end
+        most_rides_biker = ride_count_hash.key(ride_count_hash.values.max)
+        most_rides_biker
+    end
 end
--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe BikeClub do
         @biker = Biker.new("Kenny", 30)
         @biker.learn_terrain!(:gravel)
         @biker.learn_terrain!(:hills)
+        @biker2 = Biker.new("Athena", 15)
         @biker2.learn_terrain!(:gravel)
         @biker2.learn_terrain!(:hills)
-        @biker2 = Biker.new("Athena", 15)
         @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
         @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
     end
 
     describe "initialize" do
         it 'can initialize' do
-            expect(@bike_club).to be_an_instance_of(BikeClub)
+            expect(@bikeclub).to be_an_instance_of(BikeClub)
         end
         
         it 'has attributes' do
@@ -31,7 +31,7 @@ RSpec.describe BikeClub do
         it 'can add bikers' do
             @bikeclub.add_biker(@biker)
             @bikeclub.add_biker(@biker2)
-            expect(@bike_club.bikers).to eq([@biker,@biker1])
+            expect(@bikeclub.bikers).to eq([@biker,@biker2])
         end
     end
 
