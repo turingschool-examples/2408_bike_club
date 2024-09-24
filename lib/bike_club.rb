9eb1be2f603f245fc553d9a4590a4a10f4f4045f
Eric DeShetler <ejdesh@gmail.com>
2024-09-24 10:31:08 -0700

    initial commit (forgot to commit until now...)

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,3 +1,48 @@
-class BikeClub
+require './lib/biker'
 
+class BikeClub
+  attr_reader :name, :bikers
+
+  def initialize(name)
+    @name = name
+    @bikers = []
+  end
+
+  def add_biker(biker)
+    # array of Biker objects
+    @bikers << biker
+    # @bikers = @bikers.sort.uniq
+  end
+
+  def most_rides
+    # Biker object
+    @bikers.each do |biker|
+    end
+  end
+
+  def best_time(ride)
+    best_for_ride = 0
+    best_biker = nil
+
+    @bikers.each do |biker|
+      bikers_best = biker.personal_record(ride)
+      # best_for_ride = bikers_best if bikers_best > best_for_ride
+      if bikers_best > best_for_ride
+        best_for_ride = bikers_best
+        best_biker = biker
+      end
+    end
+    # Biker object
+    best_biker
+  end
+
+  def bikers_eligible(ride)
+    eligible = []
+    @bikers.each do |biker|
+      eligible << biker if biker.acceptable_terrain.include?(ride.terrain)
+    end
+    # array of Biker objects
+    eligible
+  end
 end
+
--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,17 @@
 class Ride
+  attr_reader :name, :distance, :loop, :terrain
 
+  def initialize(ride_details)
+    @name = ride_details[:name]
+    @distance = ride_details[:distance]
+    @loop = ride_details[:loop]
+    @terrain = ride_details[:terrain]
+  end
+
+  def total_distance
+    return @distance if @loop
+
+    @distance * 2
+  end
 end
+
