b214eb82bd5cfecae4fac589e4da0d031cc823ce
Wally Wallace <jrvw001@gmail.com>
2024-09-24 11:11:51 -0600

    Implement most_rides

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -11,4 +11,21 @@ class BikeClub
     @bikers << biker
     @bikers
   end
+
+  def most_rides
+    max_rides = 0
+    max_biker = nil
+
+    @bikers.each do |biker|
+      rides = 0
+      biker.rides.each do |ride, times|
+        rides += times.length
+      end
+      if rides > max_rides
+        max_rides = rides
+        max_biker = biker
+      end
+    end
+    max_biker
+  end
 end
