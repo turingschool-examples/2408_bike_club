2698873302ebbc77586d12d7b140487ae09e6215
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-24 11:17:44 -0400

    Creates methods for tests - TESTS PASS

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,21 @@
 class Ride
+    attr_reader :name,
+                :distance,
+                :loop,
+                :terrain
 
+    def initialize(ride_details)
+        @name = ride_details[:name]
+        @distance = ride_details[:distance]
+        @loop = ride_details[:loop]
+        @terrain = ride_details[:terrain]
+    end
+
+    def total_distance
+        if @loop == false
+            distance = @distance *2
+        else
+            @distance
+        end
+    end
 end
