80c7d1983ec147c34562738f666accd4060c454e
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-24 10:52:36 -0600

    added @club and tested to see if it exists

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -3,6 +3,15 @@ require './lib/ride'
 require './lib/biker'
 
 RSpec.describe BikeClub do
-  it 'exists with attributes' do
+  before(:each) do
+    @club = BikeClub.new("Bike Gang")
+  end
+
+  describe '#initialize' do
+    it 'exists with attributes' do
+      expect(@club).to be_a(BikeClub)
+      expect(@club.name).to eq("Bike Gang")
+      expect(@club.bikers).to eq([])
+    end
   end
 end
