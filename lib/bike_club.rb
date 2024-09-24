7c92a6c8f18c89a38131014d6da6067412825504
Seth Verrill <verrill.seth@gmail.com>
2024-09-24 10:43:37 -0600

    feat: Add best time method

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -15,4 +15,8 @@ class BikeClub
   def most_rides
     @bikers.max_by { |biker| biker.rides.values.sum(&:length) }
   end
+
+  def best_time(ride)
+    @bikers.min_by { |biker| biker.personal_record(ride) }
+  end
 end
