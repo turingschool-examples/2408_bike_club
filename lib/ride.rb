549064ff3940838e77d7d0a599c7888c581a38f1
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-24 09:22:22 -0600

    added attributes to ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,14 @@
 class Ride
+  attr_reader :name, :distance, :terrain, :loop
 
+  def initialize(details)
+    @name = details[:name]
+    @distance = details[:distance]
+    @terrain = details[:terrain]
+    @loop = details[:loop]
+  end
+  
+  def loop?
+    @loop
+  end
 end
