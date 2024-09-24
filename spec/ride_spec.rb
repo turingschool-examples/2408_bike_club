1bb61fce2f276af429b9fd9f934a72143f803183
Paul Knapp <ppankluap@gmail.com>
2024-09-24 10:11:54 -0500

    test: add attribute tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -1,2 +1,21 @@
 requre 'spec_helper'
 Rspec.describe Ride do 
+    before(:each) do
+        ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    end
+
+    describe 'initialize' do
+        it 'exists' do 
+            expect(ride1).to be_an_instance_of(Ride)
+        end
+    
+
+        it 'has attributes' do 
+            expect(ride1.name).to eq("Walnit Creek Trail")
+            expect(ride1.distance).to eq(10.7)
+            expect(ride1.terrain).to eq(:hills)
+            expect(ride1.loop?).to eq(false)
+            expect(ride1.total_diatance).to eq(21.4)
+        end
+    end
+end
