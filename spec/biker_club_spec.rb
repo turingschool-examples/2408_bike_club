d783b7c8aa769b73643bfde094c88b8f9739c761
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-24 12:20:40 -0500

    created tests to add bikers to club

--- spec/biker_club_spec.rb
+++ spec/biker_club_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe do
     before(:each) do
 
         @bike_club = BikeClub.new("Ride or Die", )
+        @biker = Biker.new("Kenny", 30)
+        @biker2 = Biker.new("Athena", 15)
+        @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+        @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
         end
 
 describe '#bike club' do
@@ -14,5 +18,13 @@ describe '#bike club' do
 
         expect(@bike_club).to be_an_instance_of(BikeClub)
         end
+
+    it 'can can add bikers in the club' do
+        @bike_club.add_biker(@biker1)
+        @bike_club.add_biker(@biker2)
+
+        expect(@bike_club.bikers).to eq([@biker1, @biker2])
+
     end
+end
 end
