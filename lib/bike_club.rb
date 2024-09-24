af34ce6d04ac59810f5367a7f0cd810c3df1ccf7
Seth Verrill <verrill.seth@gmail.com>
2024-09-24 11:27:59 -0600

    feat: Add group ride feature that makes a hash with start time, ride, and riders

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,10 +1,12 @@
 class BikeClub
   attr_reader :name,
-              :bikers
+              :bikers,
+              :group_rides
 
   def initialize(name)
     @name = name
     @bikers = []
+    @group_rides = []
   end
 
   def add_biker(biker)
@@ -22,5 +24,16 @@ class BikeClub
 
   def bikers_eligible(ride)
     @bikers.select { |biker| biker.acceptable_terrain.include?(ride.terrain) && ride.total_distance <= biker.max_distance }
+  end
+
+  def record_group_ride(ride)
+    eligible_riders = bikers_eligible(ride)
+    group_ride = {
+      start_time: Time.now,
+      ride: ride,
+      members: eligible_riders
+    }
+    @group_rides << group_ride
+    group_ride
   end
 end
