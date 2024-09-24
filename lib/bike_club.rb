3a5839c4b1c48872dd73451fef73c53d2ac9b919
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-24 13:38:03 -0400

    Adds best_time method - TESTS PASS

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -14,4 +14,20 @@ class BikeClub
     def most_rides
         @bikers.max_by { |biker| biker.rides.count }
     end
+
+    def best_time(ride)
+        best_biker = ""
+        best_time =  999.99
+
+        @bikers.each do |biker|
+            if biker.rides.key?(ride)
+                biker_best = biker.rides[ride].min
+                if biker_best < best_time
+                    best_time = biker_best
+                    best_biker = biker
+                end
+            end
+            return best_biker.name
+        end
+    end
 end
