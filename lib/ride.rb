87c16371d8413870d01677a3b9508fe9b83a8cea
Wally Wallace <jrvw001@gmail.com>
2024-09-24 09:57:35 -0600

    Implement ride.rb

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,22 @@
 class Ride
 
+  attr_reader :name, :distance, :terrain
+  def initialize(attributes)
+    @name = attributes[:name]
+    @distance = attributes[:distance]
+    @loop = attributes[:loop]
+    @terrain = attributes[:terrain]
+  end
+
+  def total_distance
+    if @loop 
+      @distance
+    else
+      @distance * 2
+    end
+  end
+
+  def loop?
+    @loop
+  end
 end
