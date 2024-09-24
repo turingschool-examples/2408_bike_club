376ae19857566b386562f3d71637c4e3c7cf17a2
Seth Verrill <verrill.seth@gmail.com>
2024-09-24 09:18:55 -0600

    feat: Create ride class with attributes, loop? method, and totatl distance method

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,20 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :terrain
 
+  def initialize(att)
+    @name = att[:name]
+    @distance = att[:distance]
+    @loop = att[:loop]
+    @terrain = att[:terrain]
+  end
+
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    loop? ? distance : distance * 2
+  end
 end
