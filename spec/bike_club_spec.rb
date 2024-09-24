08f00649b80bf08dc08fd7e6d49f76acfaf37df0
Alora Riley <alora.riley@gmail.com>
2024-09-24 11:55:04 -0600

    Update test to better reflect using personal record in bikers eligible test

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -76,17 +76,16 @@ RSpec.describe BikeClub do
     @biker.log_ride(@ride2, 60.9)
     @biker.log_ride(@ride2, 61.6)
 
-    expect(@biker.rides).to eq({ @ride1 => [92.5, 91.1], @ride2 => [60.9, 61.6]})
+    expect(@biker.personal_record(@ride1)).to eq(91.1)
+    expect(@biker.personal_record(@ride2)).to eq(60.9)
 
     @biker2.learn_terrain!(:gravel)
     @biker2.learn_terrain!(:hills)
-    @biker2.log_ride(@ride1, 95.0) # biker2 can't bike this distance
-    @biker2.log_ride(@ride2, 65.0) # biker2 knows this terrain and can bike this distance
+    @biker2.log_ride(@ride1, 95.0) 
+    @biker2.log_ride(@ride2, 65.0) 
 
-    expect(@biker2.rides).to eq({ @ride2 => [65.0] })
     expect(@biker2.personal_record(@ride2)).to eq(65.0)
     expect(@biker2.personal_record(@ride1)).to eq(false)
-
 
     expect(@bike_club.bikers_eligible(@ride1)).to eq([@biker])
     expect(@bike_club.bikers_eligible(@ride2)).to eq([@biker, @biker2])
