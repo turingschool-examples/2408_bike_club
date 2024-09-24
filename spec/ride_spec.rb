8068b8c61871dbc89fd2c3b10d10c74216ff3b91
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-24 09:23:06 -0600

    added test for ride_1

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -1,6 +1,14 @@
 require './lib/ride'
 
 RSpec.describe Ride do 
-  it ' exists' do
+  before(:each) do
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+  end
+    it ' exists and has attributes' do
+    expect(@ride1).to be_instance_of(Ride)
+    expect(@ride1.name).to eq("Walnut Creek Trail")
+    expect(@ride1.distance).to eq(10.7)
+    expect(@ride1.loop?).to eq(false)
+    expect(@ride1.terrain).to eq(:hills)
   end
 end
