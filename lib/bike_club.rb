17e904ccc46a06e605746b1890acebc022794b55
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-24 11:09:53 -0600

    added add biker and most rides methods

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -5,4 +5,12 @@ class BikeClub
     @name = name
     @bikers = []
   end
+
+  def add_biker(biker)
+    @bikers << biker
+  end
+
+  def most_rides
+    @bikers.max_by { |biker| biker.rides.length }
+  end
 end
