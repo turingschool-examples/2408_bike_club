8507862dcb9ce2b8313e0dba2e192da0d04e016d
Paul Knapp <ppankluap@gmail.com>
2024-09-24 12:24:45 -0500

    feat: add bikers eligable method using helper

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -5,9 +5,17 @@ class BikeClub
         @bikers = []
     end
 
-    
+    def add_biker(biker)
+        @bikers << biker
+    end
 
     def most_rides
         @bikers.max_by {|biker| biker.rides.values.sum(&:size)}
     end
+
+    def bikers_eligible(ride)
+        @bikers.select { |biker| biker.can_ride?(ride)}
+    end
+
+    
 end
