f240f00144078832554ea3c602cd2cc898c2a45e
Danielle Cardona <danielle.cardona23@gmail.com>
2024-09-24 09:14:42 -0600

    feat: add loop? and total distance methods

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,25 @@
 class Ride
+    attr_reader :name,
+                :distance,
+                :loop,
+                :terrain
 
+    def initialize(ride_info)
+        @name = ride_info[:name]
+        @distance = ride_info[:distance]
+        @loop = ride_info[:loop]
+        @terrain = ride_info[:terrain]
+    end
+
+    def loop?
+        @loop
+    end
+
+    def total_distance
+        if loop?
+            total_distance = @distance 
+        else
+            total_distance = @distance * 2
+        end
+    end
 end
