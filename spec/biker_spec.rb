4f5f8f3adf670a4f04653ba4768b6e44ed36481f
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-24 10:08:53 -0600

    added tests and rides for log_ride method

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -1,9 +1,12 @@
+require './lib/ride'
 require './lib/biker'
 require 'pry'
 
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
   
     it' exists and has attributes' do
@@ -16,9 +19,23 @@ RSpec.describe Biker do
   
   describe '#learn_terrain!' do
     it 'allows biker to learn terrain' do
-      (@biker.learn_terrain!(:gravel))
-      (@biker.learn_terrain!(:hills))
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
       expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
+  end
+
+  describe '#log_ride' do
+    it 'logs rides and times for a ride' do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.rides[@ride1]).to eq([92.5, 91.1])
+      expect(@biker.rides[@ride2]).to eq([60.9, 61.6])
     end
   end
 end
