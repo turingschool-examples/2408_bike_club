7ab29735acdf44724d57b24788568023dcbef7ad
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-24 12:49:56 -0500

    tests for best time and eligble bikers created

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,14 +1,24 @@
 class BikeClub
 
         attr_reader :name, :bikers
-      
         def initialize(name)
-          @name = name
-          @bikers = []
+            @name = name
+            @bikers = []
+          end
+        
+          def add_biker(biker)
+            @bikers << biker
+          end
+        
+          def best_time(ride)
+            @bikers.min_by { |biker| biker.personal_record(ride) }
+          end
+        
+          def bikers_eligible(ride)
+            @bikers.select do |biker|
+              biker.acceptable_terrain.include?(ride.terrain) && biker.max_distance >= ride.distance
+            end
+          end
         end
       
-        def add_biker(biker)
-          @bikers << biker
-        end
-      end
     
--- spec/biker_club_spec.rb
+++ spec/biker_club_spec.rb
@@ -7,11 +7,12 @@ RSpec.describe do
     before(:each) do
 
         @bike_club = BikeClub.new("Ride or Die", )
-        @biker = Biker.new("Kenny", 30)
+        @biker1 = Biker.new("Kenny", 30)
         @biker2 = Biker.new("Athena", 15)
         @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
         @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
-        end
+
+    end
 
 describe '#bike club' do
     it 'be an instance of' do
@@ -23,7 +24,7 @@ describe '#bike club' do
 
         expect(@bike_club.name).to eq("Ride or Die")
     end
-    
+
     it 'can can add bikers in the club' do
         @bike_club.add_biker(@biker1)
         @bike_club.add_biker(@biker2)
@@ -31,5 +32,33 @@ describe '#bike club' do
         expect(@bike_club.bikers).to eq([@biker1, @biker2])
 
     end
+
+    it 'can tell which biker has the best time for a given ride' do
+        @bike_club.add_biker(@biker1)
+        @bike_club.add_biker(@biker2)
+    
+        @biker1.log_ride(@ride1, 25.0) 
+        @biker1.log_ride(@ride2, 15.0) 
+        @biker2.log_ride(@ride1, 30.0)
+    
+        expect(@bike_club.best_time(@ride1)).to eq(@biker1)
+      end
+    
+      it 'can tell which bikers are eligible for a given ride' do
+        @bike_club.add_biker(@biker1)
+        @bike_club.add_biker(@biker2)
+
+        @biker1.learn_terrain!(:gravel)
+        @biker1.learn_terrain!(:hills)
+    
+        @biker1.log_ride(@ride1, 25.0) 
+        @biker1.log_ride(@ride2, 15.0) 
+        @biker2.log_ride(@ride1, 30.0)
+    
+
+        expect(@bike_club.bikers_eligible(@ride1)).to contain_exactly(@biker1)
+        expect(@bike_club.bikers_eligible(@ride2)).to contain_exactly(@biker1)
+      end
+
 end
 end
