1536856083a5cd4a80b7c1b003e1a43538bc779a
Seth Verrill <verrill.seth@gmail.com>
2024-09-24 09:51:02 -0600

    test: Add log rides test

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -3,6 +3,7 @@ require './spec/spec_helper'
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
@@ -21,11 +22,24 @@ RSpec.describe Biker do
     it 'can learn new terrain' do
       @biker.learn_terrain!(:gravel)
       @biker.learn_terrain!(:hills)
-      expect(@biker.acceptable_terrain).to eq([:gravel, hills])
+      expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
     end
   end
 
-
-
-
+  describe 'log rides' do
+    it 'can store ride data' do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+      test_rides = {
+        @ride1 => [92.5, 91.1],
+        @ride2 => [60.9, 61.6]
+      }
+      expect(@biker.rides).to eq(test_rides) 
+      expect(@biker2.rides).to eq({})
+    end
+  end
 end
