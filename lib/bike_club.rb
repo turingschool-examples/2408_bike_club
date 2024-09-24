48c0ed2d5607b31d473939e94166490651b2d4ee
Oscar Patterson <oepatterson12@gmail.com>
2024-09-24 13:32:46 -0400

    working onit

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,3 +1,32 @@
 class BikeClub
 
+
+    def initialize(name)
+        @name = name
+        @bikers = []
+    end
+        
+    def add_biker(biker)
+        @bikers << biker
+        @bikers
+    end
+     
+    def most_rides
+
+    end
+
+    def best_time(ride)
+
+    end
+
+    def bikers_eligible(ride)
+        eligiblebikers = []
+        @bikers.each do |biker| 
+            if biker.acceptable_terrain.include?(ride.terrain)
+                eligiblebikers << biker
+            end
+        end
+        eligiblebikers
+    end
+
 end
