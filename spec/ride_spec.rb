e795ed0d268e2a0fda90a20c91899ddb3e1af83b
Paul Knapp <ppankluap@gmail.com>
2024-09-24 10:23:29 -0500

    test: test loop function of rides

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -2,6 +2,7 @@ requre 'spec_helper'
 Rspec.describe Ride do 
     before(:each) do
         ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+        ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
     end
 
     describe 'initialize' do
@@ -16,6 +17,12 @@ Rspec.describe Ride do
             expect(ride1.terrain).to eq(:hills)
             expect(ride1.loop?).to eq(false)
             expect(ride1.total_diatance).to eq(21.4)
+        end
+
+        it 'can be a loop' do 
+            expect(ride2.loop).to eq(true)
+            expect(ride2.distance).to eq(14.9)
+            expect(ride2.total_diatance).to eq(14.9)
         end
     end
 end
