d39940e5aebf622e77df275f99d5325b45696a53
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-24 11:32:55 -0500

    created log ride method and test

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,16 +1,24 @@
 class Biker
 
-    attr_reader :name, :max_distance, :acceptable_terrain, :rides
+    attr_reader :name, :max_distance, :acceptable_terrain, :rides, :time
 
     def initialize(name, max_distance)
         @name = name
         @max_distance = max_distance
-        @rides = Hash.new(0)
+        @rides = Hash.new { |hash, key| hash[key] = [] } 
         @acceptable_terrain = []
+        @time = time
     end
 
     def learn_terrain!(terrain)
         @acceptable_terrain << terrain
-
     end
+
+    def time
+        @time
+    end
+
+    def log_ride(ride, time)
+        @rides[ride] << time
+      end
 end
--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe do
     before(:each) do
 
         @biker = Biker.new("Kenny", 30)
+        @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+        @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
     end
 
 describe '#biker' do
@@ -39,6 +41,21 @@ describe '#biker' do
         @biker.learn_terrain!(:hills)
 
         expect(@biker.acceptable_terrain).to eq ([:gravel, :hills])
+    end
+
+    it 'can log rides' do
+        @biker.log_ride(@ride1, 92.5)
+        @biker.log_ride(@ride1, 91.1)
+        @biker.log_ride(@ride2, 60.9)
+        @biker.log_ride(@ride2, 61.6)
+
+        expected_rides = {
+      @ride1 => [92.5, 91.1],
+      @ride2 => [60.9, 61.6]
+    }
+
+
+    expect(@biker.rides).to eq(expected_rides)
     end
 end
 end
